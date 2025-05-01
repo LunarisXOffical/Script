2365151c95482677b93096ca5480b16bdcb5aa02
Moonlight Hub <presidentialrealmsstudio@gmail.com>
2025-05-01 09:08:34 +0700

    Update Moonlight Hub Main.lua

--- Moonlight Hub Main.lua
+++ Moonlight Hub Main.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/MoonlightOffical/Game/refs/heads/main/GameList/Gamelist.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
