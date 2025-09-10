318a81339de80732f0098a27c29df576d4db5099
Moonlight Hub <presidentialrealmsstudio@gmail.com>
2025-09-10 21:05:03 +0700

    Update Moonlight Hub.lua

--- Moonlight Hub.lua
+++ Moonlight Hub.lua
@@ -1,7 +1,1 @@
-local Games = loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/145fb3b6894ac2930ee5f986555ec877804759132c06a2d6c2f49d7be969355c/download"))()
-
-local URL = Games[game.PlaceId]
-
-if URL then
-  loadstring(game:HttpGet(URL))()
-end
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/07f019a894ee49a882fed4ca612c4551"))()
