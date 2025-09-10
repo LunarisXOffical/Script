e8cfc8ee2ced23c614ae4e6cb2ec27e35754e99f
Moonlight Hub <presidentialrealmsstudio@gmail.com>
2025-09-10 19:04:03 +0700

    Update Moonlight Hub.lua

--- Moonlight Hub.lua
+++ Moonlight Hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/266f5efc870946269a6adb890da2b0f3"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/a7f0224b955944dabebd13146ee22413"))()
