9b1d438b2fed15f7c931b8230bc4e9583b552641
LunarisX <presidentialrealmsstudio@gmail.com>
2025-10-04 14:37:55 +0700

    Update Moonlight Hub.lua

--- Moonlight Hub.lua
+++ Moonlight Hub.lua
@@ -1,1 +1,7 @@
+local function Credits()
+-- Credits: @Stefan created https://junkie-development.de/
+-- Credits: @Nakhun12309 Made stuff.
+-- Credits: @wxyz Community Manager
+end)
+print(Credits)
 loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/d1844496f7d873d687e5f8eee91ede132b24dc84579b060d4394496e62c112f7/download"))()
